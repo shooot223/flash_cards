b3faab0c6f19adfc56ee7af6a15f6777658513bd
saeki_shuto <saeki_shuto@tenda.co.jp>
2026-02-08 20:03:05 +0900

    タイトルテーブルに編集の有効判定のためにユーザーIDを追加

--- web/laravel/database/migrations/2026_02_08_022506_create_question_title_table.php
+++ web/laravel/database/migrations/2026_02_08_022506_create_question_title_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('Question_Title', function (Blueprint $table) {
             $table->id('title_id');
             $table->string('title');
+            $table->foreignId('user_id')->constrained('User', 'user_id');
             $table->timestamps();
         });
     }
