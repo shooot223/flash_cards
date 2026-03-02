0162830c5a2ad0f1c96d08d34347e3d63d809237
saeki_shuto <saeki_shuto@tenda.co.jp>
2026-03-02 17:13:48 +0900

    追加

---
+++ web/laravel/resources/js/bootstrap.js
@@ -0,0 +1,7 @@
+import _ from 'lodash';
+window._ = _;
+
+// 必要になったら有効化（今は不要ならコメントのままでOK）
+// import axios from 'axios';
+// window.axios = axios;
+// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
