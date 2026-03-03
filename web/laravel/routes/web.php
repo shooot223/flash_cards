31bc90079f606698f55aa1cb373e12bd018acbf3
saeki_shuto <saeki_shuto@tenda.co.jp>
2026-03-03 17:21:09 +0900

    ヘッダー作成

--- web/laravel/routes/web.php
+++ web/laravel/routes/web.php
@@ -16,9 +16,7 @@ Route::get('/', function(){
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/mypage');
 });
 
 require __DIR__.'/auth.php';
