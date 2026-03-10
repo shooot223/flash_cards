390efb30de9f77cfb1e88765ed8064b383956b97
saeki_shuto <saeki_shuto@tenda.co.jp>
2026-03-10 14:55:07 +0900

    api用ルート定義

--- web/laravel/routes/api.php
+++ web/laravel/routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\QuizController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/quizzes', [QuizController::class, 'store']);
+});
