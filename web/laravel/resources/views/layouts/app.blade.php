50ceda737c1903c7efdd1fd04e7e4ea5936e989c
saeki_shuto <saeki_shuto@tenda.co.jp>
2026-03-13 15:58:34 +0900

    faviconの保存ディレクトリ修正

--- web/laravel/resources/views/layouts/app.blade.php
+++ web/laravel/resources/views/layouts/app.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
 
     <!-- favicon -->
-    <link rel="icon" href="{{ asset('img/favicon/favicon.ico') }}" sizes="any">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon/favicon-16x16.png') }}">
-    <link rel="apple-touch-icon" href="{{ asset('img/favicon/apple-touch-icon.png') }}">
-    <link rel="manifest" href="{{ asset('img/favicon/site.webmanifest') }}">
+    <link rel="icon" href="{{ asset('/img/favicon/favicon.ico') }}" sizes="any">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/img/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/img/favicon/favicon-16x16.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('/img/favicon/apple-touch-icon.png') }}">
+    <link rel="manifest" href="{{ asset('/img/favicon/site.webmanifest') }}">
 
     <!-- 共通CSS -->
     <link rel="stylesheet" href="{{ asset('css/header.css') }}">
