8e5cb70971d247fc7f01ed209accd40b82db8aff
saeki_shuto <saeki_shuto@tenda.co.jp>
2026-03-13 10:07:05 +0900

    保存用のルート定義変更

--- web/laravel/routes/api.php
+++ web/laravel/routes/api.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/quizzes', [QuizController::class, 'store']);
+    Route::post('/quiz/store', [QuizController::class, 'store']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
